ec02644c147c617c5becaa6361597338bf39c258
Shunsuke Mikami <mikami@hpcs.cs.tsukuba.ac.jp>
2011-12-01 14:20:09 +0900

    add test

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,11 @@ require 'shoulda'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'fluent-plugin-dstat'
+
+require 'fluent/test'
+require 'fluent/plugin/in_dstat'
 
 class Test::Unit::TestCase
 end
+
+
---
+++ test/plugin/test_in_dstat.rb
@@ -0,0 +1,33 @@
+require 'fluent/test'
+require 'net/http'
+
+class DstatInputTest < Test::Unit::TestCase
+  def setup
+    Fluent::Test.setup
+  end
+
+  CONFIG = %[
+    tag dstat
+    option -fcdnm 1
+  ]
+
+  def create_driver(conf=CONFIG)
+    Fluent::Test::InputTestDriver.new(Fluent::DstatInput).configure(conf)
+  end
+
+  def test_configure
+    d = create_driver
+    assert_equal "-fcdnm 1", d.instance.option
+  end
+
+  def test_emit
+    d = create_driver
+
+    d.run do
+      sleep 2
+    end
+
+    emits = d.emits
+    assert_equal true, emits.length > 0
+  end
+end
